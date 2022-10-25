9ff95084210f02c1d995ea90a150afce4bc09725
Jeremy Tupas <jeremytupas@gmail.com>
2022-10-25 17:48:48 -0500

    Add form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// Routes
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+// Post a new message. Triggered by form submitting to '/new'.
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  console.log('pushed messages');
+  res.redirect("/");
 });
 
 module.exports = router;
